217c2e358ab209f5f0767961dbd69b9dcca28f48
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-24 19:12:35 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// styles
+import classes from './css/app.module.css';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className={classes.main}>
+      <h1 className={classes.header}>GAMBA</h1>
+    </div>
+  );
 };
 
 export default App;

d2632787235efda38c0396db30d76a0e77ac408f
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-24 19:25:31 +0200

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 // styles
 import classes from './css/app.module.css';
+// components
+import Slot from './components/Slot';
 
 const App = () => {
   return (
     <div className={classes.main}>
       <h1 className={classes.header}>GAMBA</h1>
+      <div className={classes.slotContainer}></div>
     </div>
   );
 };
---
+++ src/components/Slot.jsx
@@ -0,0 +1,3 @@
+const Slot = () => {};
+
+export default Slot;

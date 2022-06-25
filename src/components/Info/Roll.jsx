e7ae4ed2385aea79b6b9a658167393d26c93bed8
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-25 17:33:52 +0200

    info layout

--- src/components/Info/Roll.jsx
+++ src/components/Info/Roll.jsx
@@ -1,3 +1,17 @@
-const Roll = () => {};
+// styles
+import classes from './css/roll.module.css';
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotate } from '@fortawesome/free-solid-svg-icons';
+
+const Roll = () => {
+  return (
+    <div className={classes.container}>
+      <span className={classes.amount}>-</span>
+      <FontAwesomeIcon icon={faRotate} className={classes.roll} />
+      <span className={classes.amount}>+</span>
+    </div>
+  );
+};
 
 export default Roll;
--- src/components/Info/Text.jsx
+++ src/components/Info/Text.jsx
@@ -1,3 +1,7 @@
-const Text = () => {};
+import classes from './css/text.module.css';
+
+const Text = () => {
+  return <div>siema mordo jak sie masz</div>;
+};
 
 export default Text;

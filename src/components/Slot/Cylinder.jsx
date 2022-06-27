2154fff07a667c14c2f480bcbc3adccbf88a2f3a
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-27 16:28:43 +0200

    styling of symbols in cyllinder

--- src/components/Slot/Cylinder.jsx
+++ src/components/Slot/Cylinder.jsx
@@ -6,7 +6,14 @@ import images from '../../utils/images.json';
 const Cylinder = () => {
   console.log(images);
   const imagesElements = images.map((image, id) => {
-    return <img key={id} src={image.path} alt={image.name} />;
+    return (
+      <img
+        key={id}
+        src={image.path}
+        alt={image.name}
+        className={classes.image}
+      />
+    );
   });
 
   return <div className={classes.cylinder}>{imagesElements}</div>;

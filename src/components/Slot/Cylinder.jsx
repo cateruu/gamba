2bd2bb1d50b65e3f39faff858730600cdef31de4
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-27 16:25:22 +0200

    rendering slot images from json

--- src/components/Slot/Cylinder.jsx
+++ src/components/Slot/Cylinder.jsx
@@ -1,8 +1,15 @@
 // styles
 import classes from './css/cylinder.module.css';
+// utils
+import images from '../../utils/images.json';
 
 const Cylinder = () => {
-  return <div className={classes.cylinder}></div>;
+  console.log(images);
+  const imagesElements = images.map((image, id) => {
+    return <img key={id} src={image.path} alt={image.name} />;
+  });
+
+  return <div className={classes.cylinder}>{imagesElements}</div>;
 };
 
 export default Cylinder;

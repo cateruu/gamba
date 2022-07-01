abfd1f13ca800dc9b596a30c6323b8bf7d7672e5
Paweł Kromołowski <cater981pl@gmail.com>
2022-07-01 15:43:00 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // styles
 import classes from './css/app.module.css';
 // components
+import Reels from './components/Slot/Reels/Reels';
+import Info from './components/Slot/Info/Info';
+import HelpBtn from './components/Help/HelpBtn';
 import Help from './components/Help/Help';
 import Slot from './components/Slot/Slot';
 
-const App = () => {
-  const [showHelp, setShowHelp] = useState(false);
-  const [credits, setCredits] = useState(200);
+export const RollContext = React.createContext();
 
-  const help = () => {
-    setShowHelp((prevState) => !prevState);
-  };
+const App = () => {
+  const [firstTime, setFirstTime] = useState(true);
+  const [isRolling, setIsRolling] = useState(false);
+  const cylinders = [useRef(), useRef(), useRef()];
+  const newReels = useRef(false);
+  const [showHelp, setShowHelp] = useState(false);
 
   return (
     <main className={classes.main}>
-      {showHelp ? (
-        <Help handleClick={help} />
-      ) : (
-        <Slot handleClick={help} credits={credits} setCredits={setCredits} />
-      )}
+      {showHelp}
+      <Slot />
     </main>
   );
 };
